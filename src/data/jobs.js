0524e875ffb922d821b6416a773d9feb409b2375
Gustavo Bessa <gustavoux@yahoo.com.br>
2024-01-02 17:00:14 -0300

    feat: increase GD job description

--- src/data/jobs.js
+++ src/data/jobs.js
@@ -1,35 +1,40 @@
 const jobs = [
   {
-    name: 'Desenvolvedor Fullstack',
-    description: 'Responsabilidades: \n•	Participei do desenvolvimento dos sistemas de barter de clientes externos.',
-    type: 'DEV',
-    company: 'Grão Direto',
-    start: 'Fevereiro de 2022',
-    end: ' - '
+    name: "Desenvolvedor Fullstack",
+    description:
+      "Responsabilidades: \n•	Participei do desenvolvimento dos sistemas de barter de clientes externos.\n•	Participei do desenvolvimento de plataformas web responsivas para a comercialização digital de grãos\n•	Desenvolvi uma funcionalidade de comunicação WhatsApp similar a um chatbot utilizando a ferramenta Twilio Studio Flow\n•	Atuei com desenvolvimento ágil utilizando o framework Scrum\n•	Tive contato com infraestrutura orientada a cloud com integração contínua (CI/CD) através do Github actions\n•	Liderei uma equipe com outros 4 desenvolvedores, representando-os em contatos com os stakeholders dos projetos e participando ativamente das tomadas de decisão sobre o desenvolvimento\n•	Ministrei iniciativas de educação interna e mentorias, sobre funcionamento e sintaxe dos frameworks que utilizamos, qualidade de código e testes unitários",
+
+    type: "DEV",
+    company: "Grão Direto",
+    start: "Fevereiro de 2022",
+    end: " - ",
   },
   {
-    name: 'Desenvolvedor Fullstack',
-    description: 'Responsabilidades: \n•	Implementei e mantive funcionalidades no Bigdim, o produto da empresa.\n•	Desenvolvi com a equipe um aplicativo em ReactNative, que as empresas clientes do Bigdim acessam para gerir os pedidos recebidos via delivery.\n•	Consumi APIs do Google Maps e MapBox para implementar cobrança de taxa de entrega por geolocalização (por distância em km).',
-    type: 'DEV',
-    company: 'M-DU Tecnologia',
-    start: 'Agosto de 2020',
-    end: 'Janeiro de 2022'
+    name: "Desenvolvedor Fullstack",
+    description:
+      "Responsabilidades: \n•	Implementei e mantive funcionalidades no Bigdim, o produto da empresa.\n•	Desenvolvi com a equipe um aplicativo em ReactNative, que as empresas clientes do Bigdim acessam para gerir os pedidos recebidos via delivery.\n•	Consumi APIs do Google Maps e MapBox para implementar cobrança de taxa de entrega por geolocalização (por distância em km).",
+    type: "DEV",
+    company: "M-DU Tecnologia",
+    start: "Agosto de 2020",
+    end: "Janeiro de 2022",
   },
   {
-    name: 'Vendedor',
-    description: 'Responsabilidades: \n• Venda de produtos.\n• Atendimento aos clientes.\n• Controle do estoque.\n• Compra de mercadorias.\n• Entrega dos materiais vendidos aos clientes.',
-    type: 'SALES',
-    company: 'MJ Barbosa',
-    start: 'Janeiro de 2016',
-    end: 'Agosto de 2020'
+    name: "Vendedor",
+    description:
+      "Responsabilidades: \n• Venda de produtos.\n• Atendimento aos clientes.\n• Controle do estoque.\n• Compra de mercadorias.\n• Entrega dos materiais vendidos aos clientes.",
+    type: "SALES",
+    company: "MJ Barbosa",
+    start: "Janeiro de 2016",
+    end: "Agosto de 2020",
   },
   {
-    name: 'Operador de Caixa',
-    description: 'Responsabilidades: \n• Operador de caixa do supermercado.\n• Controle do estoque.\n• Reposição das mercadorias.',
-    type: 'REGISTER',
-    company: 'Barbosa Supermercados',
-    start: 'Abril de 2014',
-    end: 'Novembro de 2015'
+    name: "Operador de Caixa",
+    description:
+      "Responsabilidades: \n• Operador de caixa do supermercado.\n• Controle do estoque.\n• Reposição das mercadorias.",
+    type: "REGISTER",
+    company: "Barbosa Supermercados",
+    start: "Abril de 2014",
+    end: "Novembro de 2015",
   },
 ];
 export default jobs;
